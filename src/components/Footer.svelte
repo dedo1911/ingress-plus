26283f8e58509435175dd144b9026400198fdd77
Dario Emerson <dedodj@gmail.com>
2023-07-31 13:59:09 +0200

    feat(footer): add github link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,11 @@
 <footer>
   <p>Not officially affiliated with Ingress or Niantic Labs! All images shown on this website are of their respective owners.</p>
+  <p>
+    Made with <img src="/heart.svg" alt="Love"> with 
+    <a href="https://svelte.dev" target="_blank"><img src="/svelte.svg" alt="Svelte" /> Svelte</a> and 
+    <a href="https://pocketbase.io" target="_blank"><img src="/pocketbase.svg" alt="PocketBase"> PocketBase</a> &mdash; 
+    <a href="https://github.com/dedo1911/ingress-dedo1911" target="_blank"><img src="/github.svg" alt="GitHub"> Contribute</a>
+  </p>
 </footer>
 
 <style>
@@ -11,6 +17,21 @@
     justify-content: center;
     text-align: center;
     background: #3b1e5f;
+    flex-direction: column;
+  }
+  p:first-child {
     font-size: small;
+  }
+  img {
+    height: 1.25em;
+    vertical-align: sub;
+    margin: 0 0.25em;
+  }
+  a:link, a:visited, a:hover, a:active {
+    color: #FFFFFF;
+    text-decoration: none;
+  }
+  a:hover {
+    font-weight: bold;
   }
 </style>
