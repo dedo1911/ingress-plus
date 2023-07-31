23868738235f8d7830326acb0b1274edc2786215
Dario Emerson Di Sante <d.disante@cloudphoenix.it>
2023-08-01 01:22:39 +0200

    feat(layout): custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<div>
+  <h1>{$page.status}</h1>
+  <p>{$page.error.message}</p>
+</div>
+
+<style>
+  div {
+    background: url('/onyx_error_badge.png') center no-repeat;
+    background-size: contain;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    height: 192px;
+    margin: 1em auto;
+  }
+  h1, p {
+    margin: 0.25em;
+  }
+</style>
