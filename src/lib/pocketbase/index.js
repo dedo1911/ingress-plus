f1be895c8d19111bff8a96b07f77625ffbcfc379
Dario Emerson Di Sante <d.disante@cloudphoenix.it>
2023-08-01 12:41:18 +0200

    feat: package updates

--- src/lib/pocketbase/index.js
+++ src/lib/pocketbase/index.js
@@ -2,7 +2,7 @@ import { dev } from '$app/environment'
 import PocketBase from 'pocketbase'
 
 export const serverAddress = dev
-  ? 'http://127.0.0.1:8090'
+  ? 'https://ingress.dedo1911.xyz'
   : window.location.origin
 
 export const client = new PocketBase(serverAddress)
