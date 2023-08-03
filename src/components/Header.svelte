e724107fc041fc2296d0c7a9ad43a44e0060dfbd
Dario Emerson Di Sante <d.disante@cloudphoenix.it>
2023-08-03 11:42:59 +0200

    feat(layout): add mobile menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,15 @@
 <script>
   import { onMount } from 'svelte'
+  import { slide } from 'svelte/transition'
   import { client } from '$lib/pocketbase'
   import { authData, ownedBadges } from '$lib/stores'
 
+  let menuOpen = false
+
+  const toggleMenu = () => (menuOpen = !menuOpen)
+
   const login = async () => {
+    menuOpen = false
     const user = await client.collection('users').authWithOAuth2({ provider: 'google' })
     
     if (client.authStore.isValid) {
@@ -17,12 +23,16 @@
     await refreshOwnedBadges()
   }
   const logout = () => {
+    menuOpen = false
     client.authStore.clear()
     authData.set({ isValid: false })
     ownedBadges.set([])
   }
   
-  const openTelegram = () => (window.location.href = "https://t.me/ingressbadges")
+  const openTelegram = () => {
+    menuOpen = false
+    window.location.href = "https://t.me/ingressbadges"
+  }
 
   const refreshOwnedBadges = async () => {
     const owned = await client.collection('user_badges').getFullList()
@@ -44,10 +54,20 @@
       <h1>Ingress Badges</h1>
     </a>
   </div>
-  <div data-nav="mobile">
-    
-  </div>
-  <div data-nav="large">
+  <nav data-nav="mobile">
+    <span class:open={menuOpen} on:click={toggleMenu} />
+    {#if menuOpen}
+      <ul transition:slide>
+        <li on:click={openTelegram}><img src="/telegram.svg" alt="Telegram" /> Telegram</li>
+        {#if $authData.isValid }
+          <li on:click={logout}><img src="/user.svg" alt="{$authData.model.display_name}" /> Logout</li>
+        {:else}
+          <li on:click={login}><img src="/user.svg" alt="Login" /> Login</li>
+        {/if}
+      </ul>
+    {/if}
+  </nav>
+  <nav data-nav="large">
     <ul>
       <li on:click={openTelegram}><img src="/telegram.svg" alt="Telegram" /> Telegram</li>
       {#if $authData.isValid }
@@ -56,7 +76,7 @@
         <li on:click={login}><img src="/user.svg" alt="Login" /> Login</li>
       {/if}
     </ul>
-  </div>
+  </nav>
 </header>
 
 <style>
@@ -68,13 +88,14 @@
     height: 100px;
     color: #FFF;
   }
-  div {
+  div, nav {
     display: flex;
     align-items: center;
     padding: 0 1em;
   }
   header a {
     display: flex;
+    align-items: center;
   }
   div:first-child img {
     height: 75px;
@@ -84,12 +105,12 @@
     font-size: 2em;
     text-align: center;
   }
-  div[data-nav="large"] ul {
+  nav ul {
     list-style: none;
     display: flex;
     align-items: center;
   }
-  div[data-nav="large"] ul li {
+  nav ul li {
     min-width: 75px;
     width: 120px;
     text-align: center;
@@ -99,20 +120,71 @@
     align-items: center;
     justify-content: center;
     border-bottom: 1px solid rgba(255, 255, 255, 0);
+    transition: border 0.3s ease-in-out;
   }
-  div[data-nav="large"] ul li:hover {
+  nav ul li:hover {
     border-bottom: 1px solid rgba(255, 255, 255, 1);
   }
-  div[data-nav="large"] ul li img {
+  nav ul li img {
     height: 1.5em;
     width: 1.5em;
     margin-right: 0.5em;
     border-radius: 6px;
   }
 
+  nav[data-nav="large"] {
+    display: flex;
+  }
+  nav[data-nav="mobile"] {
+    display: none;
+  }
+
+  nav[data-nav="mobile"] span {
+    height: 50px;
+    width: 50px;
+    background-image: url('/menu.png');
+    background-size: contain;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+  }
+  nav[data-nav="mobile"] span.open {
+    background-image: url('/close.png');
+  }
+  nav[data-nav="mobile"] ul {
+    position: absolute;
+    top: 75px;
+    background: rgb(47,55,97);
+    background: linear-gradient(0deg, rgba(47,55,97,0.9) 0%, rgba(17,20,35,1) 50%, rgba(0,0,0,1) 100%);
+    flex-direction: column;
+    margin: 0;
+    width: 100%;
+    left: 0;
+    z-index: 1;
+    padding: 1em 0;
+    border-bottom: 3px double #5e5a75;
+    border-radius: 0 0 8px 8px;
+  }
+  nav[data-nav="mobile"] ul li {
+    padding: 1em 0.5em;
+  }
+
   @media (max-width: 800px) {
-    div[data-nav="large"] {
+    nav[data-nav="large"] {
       display: none;
+    }
+    nav[data-nav="mobile"] {
+      display: flex;
+    }
+    header {
+      height: 75px;
+    }
+    div:first-child img {
+      height: 55px;
+      width: 55px;
+      margin-right: 0.5em;
+    }
+    h1 {
+      font-size: 1.5em;
     }
   }
 </style>
