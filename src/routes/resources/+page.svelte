0e21356ef21253c260a5ca14d58c62fcb872c2f1
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2024-04-16 18:54:27 +0200

    Update +page.svelte
    added initial set of resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,14 +1,101 @@
 <svelte:head>
-    <title>Ingress Plus &middot; Terms of use</title>
+    <title>Ingress Plus &middot; Resources</title>
 </svelte:head>
 <div>
 
     <h1>Resources</h1>
-    <h3>This page will list commonly used resources that will help you get the best out of Ingress!</h3>
+    <p>This page will list commonly used resources that will help you get the best out of Ingress! If you have a suggestion for this list <a target="_blank" rel="noopener noreferrer" href="https://t.me/+HCFL_U0Xq2lhOTI0">join the Ingress.Plus chat on Telegram</a> or <a target="_blank" rel="noopener noreferrer" href="https://github.com/dedo1911/ingress-plus">edit this site via GitHub</a> and create a pull request!</p>
+    <hr>
+    <h2>Websites</h2>
+    <ul>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://ingress.plus">Ingress.Plus</a>
+            <ul>
+                <li>The very website you are already on :)</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://ingress.com">Ingress</a>
+            <ul>
+                <li>The official Websites of Ingress, includes<ul>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://ingress.com/news">News &amp; Events</a>
+                            <ul>
+                                <li>The main place to get game news</li>
+                            </ul>
+                        </li>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://intel.ingress.com/">Intel Map</a>
+                            <ul>
+                                <li>Use the Intel map to see the status of Portals, Links and Fields globally</li>
+                            </ul>
+                        </li>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://ingress.com/support">Support</a>
+                            <ul>
+                                <li>Read through the knowledge base or open a support ticket</li>
+                            </ul>
+                        </li>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://missions.ingress.com/">Mission Creator</a>
+                            <ul>
+                                <li>Create Missions that Agents can play once they have been approved</li>
+                            </ul>
+                        </li>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@Ingress">YouTube</a>
+                            <ul>
+                                <li>YouTube Channel which receives new videos from time to time</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://wayfarer.nianticlabs.com/">Niantic Wayfarer</a>
+            <ul>
+                <li>Review and edit your Portal Submission before they go into voting</li>
+                <li>Review other submissions and earn points towards your Recon medal in Ingress</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://iitc.app/">IITC-CE</a>
+            <ul>
+                <li>Script to allow plugins for the Intel Map</li>
+                <li>Works for Desktop, Android and iOS</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://bannergress.com">Bannergress</a>
+            <ul>
+                <li>Lists and searches through Mission banners submitted by Agents</li>
+                <li>Map can also be used to see which banners are around you</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://www.openbanners.org/">OpenBanners</a>
+            <ul>
+                <li>Alternative, open-source front-end for Bannergress's back-end</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://fevgames.net/category/ingress/">FevGames</a>
+            <ul>
+                <li>News site that covers Ingress from time to time</li>
+                <li>Mainly known <a target="_blank" rel="noopener noreferrer" href="https://fevgames.net/ingressfs/">for organizing the Ingress First Saturday events</a></li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://www.ingress-cards.com/">Ingress Biocards</a>
+            <ul>
+                <li>Database of Biocards and Swag, both official and unofficial</li>
+                <li>Also has <a target="_blank" rel="noopener noreferrer" href="https://www.ingress-cards.com/badges/">a handy guide</a> to check which Biocard gives what medal</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://softspot.nl/ingress/bannercropper/">Mission Banner Cropper</a>
+            <ul>
+                <li>Crops and splits images so that they can be used as a banner</li>
+                <li>Defacto successor of this <a target="_blank" rel="noopener noreferrer" href="https://www.giacintogarcea.com/ingress/tools/missionset/">Mission Banner Cropper</a> as it hasn't been updated in a while</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://wayfarer.tools/">Unofficial Wayfarer Tools</a>
+            <ul>
+                <li>A collection of tools designed to make Wayfarer better</li>
+            </ul>
+        </li>
+    </ul>
     <hr>
     <h2>Telegram</h2>
     <h3>Ingress.Plus</h3>
-    <p>You can find the Telegram channels and group for our site here!</p>
+    <p>You can find the Telegram channels and group for our own site here!</p>
     <ul>
         <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/Ingress_Plus">Ingress.Plus - @Ingress_Plus</a>
             <ul>
@@ -39,6 +126,12 @@
                 <li>Official Ingress Telegram channel</li>
             </ul>
         </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/thiaNtoasty">NianticThia Adventures</a>
+            <ul>
+                <li>Personal channel of Community Operations NianticThia</li>
+                <li>Posts about some things the official Ingress channel might not</li>
+            </ul>
+        </li>
         <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/IUENG">Ingress Updates [ENG] - @IUENG</a>
             <ul>
                 <li>Crossfaction</li>
@@ -61,10 +154,53 @@
             </ul>
         </li>
     </ul>
+    <h3>Groups</h3>
+    <p>This section lists crossfaction groups with different purposes that you can join.</p>
+    <ul>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/+TyFsSXWUopcJCyca">Discussions - Ingress Updates [ENG]</a>
+            <ul>
+                <li>2.5k members</li>
+                <li>Discussion group connected to the Ingress Updates [ENG] channel, but is used as a general Ingress chat among Agents</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/+QQ5HRJForsTWhqc1">Operation: Essex</a>
+            <ul>
+                <li>600 members</li>
+                <li>Focused on the storyline and lore of Ingress, speaking “in character” as if Ingress were real (no 4th wall break)</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/+BDQO8vpRm8wwNmE0">SwagAnon XF Global</a>
+            <ul>
+                <li>1.1k members</li>
+                <li>Group for discussion about official and unofficial Ingress Swag as well as a place to find someone that sells their stuff</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/+MfOnSjM6-3llNjkx">Mission Banners, Oh My! (Global XFAC [ENG])</a>
+            <ul>
+                <li>1.4k members</li>
+                <li>Use and share designs for Mission Banners with other Agents</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/+QED-TBCaLHB16PgY">[Global] #MissionProject</a>
+            <ul>
+                <li>1.8k members</li>
+            </ul>
+        </li>
+    </ul>
     <h3>Bots</h3>
     <p>This section lists Telegram bots that are great to have in your repertoire.</p>
     <p>Note: “Inline” bots can be used in any chat by typing the username of the bot at the beginning of the message. Some bots work inline only.</p>
     <ul>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/drone_reminder_bot">Ingress Drone Reminder</a>
+            <ul>
+                <li>Reminds you to move your drone after an hour (or whatever the current cooldown during an event is)</li>
+            </ul>
+        </li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/Bannergressbot">Bannergress Bot</a>
+            <ul>
+                <li>Can be used to search through the database or show you banners near your location</li>
+            </ul>
+        </li>
         <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/IngressProfileBot">IngressProfileBot - @IngressProfileBot</a> - Inline only<ul>
                 <li>Type an Agents name and instantly share a link to their profile in the chat</li>
             </ul>
