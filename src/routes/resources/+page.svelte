c7104e2334273c45ea447e0ce39f815288977a05
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2024-04-16 15:32:12 +0200

    Create +page.svelte

---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+    <title>Ingress Plus &middot; Terms of use</title>
+</svelte:head>
+
+<p>Test text</p>
+
+<style>
+  div {
+    max-width: 1000px;
+    margin: auto;
+    padding: 0 1em;
+    line-height: 1.2em;
+  }
+  p {
+    text-align: justify;
+  }
+  h1,
+  h2,
+  h3 {
+    text-align: center;
+  }
+</style>
