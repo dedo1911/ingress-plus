d0c76b1807363b8b0e5bf10f451dc94b4743ed31
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2024-04-16 15:43:41 +0200

    Update +page.svelte
    added first telegram channels

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,9 +1,43 @@
 <svelte:head>
     <title>Ingress Plus &middot; Terms of use</title>
 </svelte:head>
+<div>
 
-<p>Test text</p>
+<h1>Community resources</h1>
 
+<hr>
+
+<h2>Telegram</h2>
+<h3>News channels</h3>
+<ul>
+    <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/NianticOfficial">Ingress - @NianticOfficial</a>
+        <ul>
+            <li>Official Ingress Telegram channel</li>
+        </ul>
+    </li>
+    <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/IUENG">Ingress Updates [ENG] - @IUENG</a>
+        <ul>
+            <li>Crossfaction</li>
+            <li>13.5k members</li>
+            <li>General Ingress news such as events and updates about the Scanner and Anomalies</li>
+        </ul>
+    </li>
+    <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/IEToday">Enlightened Today - @IEToday</a>
+        <ul>
+            <li>ENL focused</li>
+            <li>6.5k members</li>
+            <li>Focus on news about ENL operations, swag, events</li>
+        </ul>
+    </li>
+    <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/RGNNticker">RGNNticker - @RGNNticker</a>
+        <ul>
+            <li>RES focused</li>
+            <li>8.9k members</li>
+            <li>Focus on news about RES operations, swag, events as well as general news</li>
+        </ul>
+    </li>
+
+</div>
 <style>
   div {
     max-width: 1000px;
