81b4674d4d5dd265ad7c363a2397a8c984076bab
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2024-06-18 22:11:34 +0200

    added verification badge

---
+++ src/routes/verify/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+      import { authData } from '$lib/stores'
+
+    let verification = '';
+     $: {
+       verification = $authData?.baseModel?.verification || '';
+       if (verification === '') {
+        verification = 'none';
+      }
+    }
+
+</script>
+
+<p><img src="/images/verification/verified.svg"/>Your Verification level is "<b>{verification.toUpperCase()}</b>"</p>
+
+<style>
+p {
+    text-align: center;
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+  .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  img {
+      height: 1em;
+      vertical-align: sub;
+      margin: 0 0.25em;
+  }
+</style>
