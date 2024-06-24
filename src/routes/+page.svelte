31c119c6dcf78d5d1d563b57fe8e8c655f51c1a7
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2024-06-24 10:59:06 +0200

    homepage wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,9 @@
     Users of this site can create a profile with badges they own in the game to share, submit Ingress media
     and raise bug reports for community awareness (ensure you raise in app firstly). <i>We are not affiliated with Ingress or Niantic.</i></p>
 <p>Badges</p>
+<img src="/images/homepage/cat_badges.jpg">
 <p>Mediagress</p>
+<img src="/images/homepage/cat_mediagress.png">
 <p>Bug Tracker</p>
 <p>Resources</p>
 
@@ -16,6 +18,14 @@
         text-align: center;
         margin: 2em auto;
         max-width: 800px;
+    }
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin: 2em auto;
+        width: 90%;
+        max-width: 700px;
     }
 
 </style>
