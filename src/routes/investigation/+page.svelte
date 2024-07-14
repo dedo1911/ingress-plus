35c3a79ba15a7f243d695c7b3ffda5bc513b8dba
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2024-07-14 23:31:18 +0200

    lore placeholder

--- src/routes/investigation/+page.svelte
+++ src/routes/investigation/+page.svelte
@@ -5,8 +5,8 @@
 <h2>The Investigation</h2>
 <h3>A complete story recap of Ingress</h3>
 
-<p>I will lose my mind before I complete this section. /.ixm</p>
-<p>I will also lose my mind if I hear one more "Investigation is mispelled" (Thanks for the heads up yall &lt;3) /.ixm</p>
+<p>I will lose my mind before I complete this.</p>
+<p>Until then, this page will remain a placeholder. /.ixm</p>
 
 <style>
     p {
