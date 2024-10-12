02f468b36a355e1ca9c1bdca5a398d2c18af3150
Dario Emerson <dedodj@gmail.com>
2024-10-12 12:12:43 +0200

    feat: fix verification page layout

--- src/routes/verify/+page.svelte
+++ src/routes/verify/+page.svelte
@@ -1,67 +1,76 @@
 <script>
-      import { authData } from '$lib/stores'
+    import { authData } from "$lib/stores";
 
-      let username = ''
-      let faction = ''
-      let verification = '';
-      let userId = '';
-$: {
-       username = $authData?.baseModel?.username || '';
-       if (username === '') {
-        username = 'NONE';}
-
-       faction = $authData?.baseModel?.faction || '';
-       if (faction === '') {
-        faction = 'NOT SET';}
-
-       verification = $authData?.baseModel?.verification || '';
-       if (verification === '') {
-        verification = 'NONE';}
-
-        userId = $authData?.baseModel?.id || '';
-          if (userId === '') {
-          userId = 'NONE';}
-    }
-
+    let username = $authData?.baseModel?.username || "NONE";
+    let faction = $authData?.baseModel?.faction || "NOT SET";
+    let verification = $authData?.baseModel?.verification || "NONE";
+    let userId = $authData?.baseModel?.id || "NONE";
 </script>
 
 <svelte:head>
     <title>Ingress Plus &middot; Verification</title>
 </svelte:head>
 
-{#if $authData.isValid }
-<img class="profilepicture" src="{$authData?.baseModel?.avatar.slice(0, -6)}" /><br>
-<p>Your username is "<b>{username}</b>".
-<p>Your faction is <b>{faction.toUpperCase()}</b>.
-<p><img class="badge" src="/images/verification/verified.svg"/>Your Verification level is "<b>{verification.toUpperCase()}</b>".</p>
-<p>Your User ID is "<b>ING+{userId}</b>".</p>
+<div>
+{#if $authData.isValid}
+    <img
+        class="profilepicture"
+        src={$authData?.baseModel?.avatar.slice(0, -6)}
+        alt={username}
+    /><br />
+    <p>Your username is "<b>{username}</b>".</p>
+    <p>Your faction is <b>{faction.toUpperCase()}</b>.</p>
+    <p>
+        <img
+            class="badge"
+            src="/images/verification/verified.svg"
+            alt={verification}
+        />Your Verification level is "<b>{verification.toUpperCase()}</b>".
+    </p>
+    <p>Your User ID is "<b>ING+{userId}</b>".</p>
 {:else}
-<p style="margin-top:2em;">You are currently not logged in. Please log in first.<p>
+    <p style="margin-top:2em;">
+        You are currently not logged in. Please log in first.
+    </p>
 {/if}
-<hr>
-<p>Verification is only done manually and at the discretion of the admins at the moment and can not be requested.</p>
-<br>
-<p>If instructed, make sure that your username and faction above match your Ingress account, keep your User ID ready and follow further instructions provided by the admins.</p>
+<hr />
+<p>
+    Verification is only done manually and at the discretion of the admins at
+    the moment and can not be requested.
+</p>
+<br />
+<p>
+    If instructed, make sure that your username and faction above match your
+    Ingress account, keep your User ID ready and follow further instructions
+    provided by the admins.
+    </p>
+</div>
 
 <style>
-p {
-    text-align: center;
+div {
+    max-width: 1000px;
     margin: auto;
-    max-width: 800px;
+    padding: 0 1em;
+    line-height: 1.2em;
+    margin-top: 2em;
+  }
+    p {
+        text-align: center;
+        margin: auto;
+        max-width: 800px;
     }
 
-  .badge {
-      height: 1em;
-      vertical-align: sub;
-      margin: 0 0.25em;
-  }
-  .profilepicture {
-      border: 1px solid white;
-      margin-top:2em;
-      display: block;
-      width: 200px;
-      margin-left: auto;
-      margin-right: auto;
-  }
-
+    img.badge {
+        height: 1em;
+        vertical-align: sub;
+        margin: 0 0.25em;
+    }
+    img.profilepicture {
+        display: block;
+        margin: auto;
+        max-width: 200px;
+        border-radius: 20%;
+        box-shadow: #9593c3 0px 0px 5px 1px;
+        border-radius: 6px;
+    }
 </style>
