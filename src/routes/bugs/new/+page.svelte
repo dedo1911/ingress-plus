5de057bc0868411c22b8157fecf20886f8237490
Dario Emerson <dedodj@gmail.com>
2024-10-20 14:32:24 +0200

    feat: upload attachments with bug reports

--- src/routes/bugs/new/+page.svelte
+++ src/routes/bugs/new/+page.svelte
@@ -1,121 +1,167 @@
 <script>
-  import { toast } from '@zerodevx/svelte-toast'
-  import { Carta, MarkdownEditor } from 'carta-md'
-  import DOMPurify from 'isomorphic-dompurify'
-  import { pb } from '$lib/pocketbase'
-  import { goto } from '$app/navigation'
-  import { authData } from '$lib/stores'
+  import { toast } from "@zerodevx/svelte-toast";
+  import { Carta, MarkdownEditor } from "carta-md";
+  import DOMPurify from "isomorphic-dompurify";
+  import { pb } from "$lib/pocketbase";
+  import { goto } from "$app/navigation";
+  import { authData } from "$lib/stores";
 
-  let title = ''
-  let ingressVersion = ''
-  let description = ''
-  let ReproductionSteps = ''
-  let attachments = ''
+  let title = "";
+  let ingressVersion = "";
+  let description = "";
+  let ReproductionSteps = "";
+  let attachments;
 
-    const carta = new Carta({
-    sanitizer: DOMPurify.sanitize
-  })
+  const carta = new Carta({
+    sanitizer: DOMPurify.sanitize,
+  });
 
-  $: if ($authData.isValid !== true) goto('/bugs')
+  $: if ($authData.isValid !== true) goto("/bugs");
 
   const publishReport = async () => {
-
     //Title lengh validation
     if (title.length < 3) {
-      toast.push('Title needs to be at least 3 characters long', { classes: ['errorToast'] })
-      return
+      toast.push("Title needs to be at least 3 characters long", {
+        classes: ["errorToast"],
+      });
+      return;
     }
     if (title.length > 256) {
-      toast.push('Title needs to be less than 256 characters long', { classes: ['errorToast'] })
-      return
+      toast.push("Title needs to be less than 256 characters long", {
+        classes: ["errorToast"],
+      });
+      return;
     }
 
     //Ingress version validation
-    if (ingressVersion.length > 0 && !/\b\d+\.\d+\.\d+\b/.test(ingressVersion)) {
-      toast.push('Ingress version needs to follow the format "2.XXX.X"', { classes: ['errorToast'] })
-      return
+    if (
+      ingressVersion.length > 0 &&
+      !/\b\d+\.\d+\.\d+\b/.test(ingressVersion)
+    ) {
+      toast.push('Ingress version needs to follow the format "2.XXX.X"', {
+        classes: ["errorToast"],
+      });
+      return;
     }
 
     //Description length validation
     if (description.length < 10) {
-      toast.push('Description needs to be at least 10 characters long', { classes: ['errorToast'] })
-      return
+      toast.push("Description needs to be at least 10 characters long", {
+        classes: ["errorToast"],
+      });
+      return;
     }
     if (description.length > 1024) {
-      toast.push('Description needs to be less than 1024 characters long. If more space is needed, continue in the comments.', { classes: ['errorToast'] })
+      toast.push(
+        "Description needs to be less than 1024 characters long. If more space is needed, continue in the comments.",
+        { classes: ["errorToast"] },
+      );
+      return;
+    }
+
+    if (Array.from(attachments || []).length > 5) {
+      toast.push('You can only upload up to 5 images.', { classes: ['errorToast'] })
       return
     }
 
-    //TODO: Image validation
-    //Image validation (Only allow upload if max. 5 images)
-    //if (parseInt(attachments.get(0).files.length) > 5) {
-    //toast.push('You can only upload up to 5 images.', { classes: ['errorToast'] })
-    //return
-    //}
+    const formData = new FormData()
+    formData.append('reporter', pb.authStore.model.id)
+    formData.append('status', '3jxhx1qd595ldd0')
+    formData.append('title', title)
+    formData.append('description', description)
+    formData.append('ingress_version', ingressVersion)
+    formData.append('reproduction_steps', ReproductionSteps)
+    for (const file of attachments || []) formData.append('attachments', file)
+    const entry = await pb.collection("bug_reports").create(formData);
+    goto(`/bugs/${entry.id}`);
+  };
 
-    const entry = await pb.collection('bug_reports').create({
-      reporter: pb.authStore.model.id,
-      status: '3jxhx1qd595ldd0',
-      title,
-      description,
-      ingress_version: ingressVersion,
-      reproduction_steps: ReproductionSteps,
-      attachments
-    })
-    goto(`/bugs/${entry.id}`)
+  $: if (attachments) {
+    console.log(attachments);
   }
+
+  const removeAttachment = (index) => {
+    const newAttachments = Array.from(attachments)
+    newAttachments.splice(index, 1)
+    attachments = newAttachments
+  };
 </script>
 
 <h1>New Bug Report</h1>
 
 <div>
-  <input type="text" placeholder="Title" bind:value={title} />
+  <input type="text" name="title" placeholder="Title" bind:value={title} />
 
-  <input type="text" placeholder="Ingress version (2.XXX.X)" bind:value={ingressVersion} />
+  <input
+    type="text"
+    name="ingressVersion"
+    placeholder="Ingress version (2.XXX.X)"
+    bind:value={ingressVersion}
+  />
 
   <hr />
 
-<h2>Description of the bug</h2>
-  <MarkdownEditor {carta} theme="ingressplus" bind:value={description} placeholder="Type here..." />
+  <h2>Description of the bug</h2>
+  <MarkdownEditor
+    {carta}
+    name="description"
+    theme="ingressplus"
+    bind:value={description}
+    placeholder="Type here..."
+  />
   <hr />
 
-<h3>Reproduction steps (optional)</h3>
-  <MarkdownEditor {carta} theme="ingressplus" bind:value={ReproductionSteps} placeholder="Open Ingress, then..." />
-<hr>
+  <h3>Reproduction steps (optional)</h3>
+  <MarkdownEditor
+    {carta}
+    name="reproductionSteps"
+    theme="ingressplus"
+    bind:value={ReproductionSteps}
+    placeholder="Open Ingress, then..."
+  />
+  <hr />
 
-<!-- TODO: Sending attachments broken, didn't understand documentation: https://pocketbase.io/docs/files-handling ^^' -->
-{#if $authData?.baseModel?.verification != ''}
-  <h3>Screenshots (optional)</h3>
-    <p>You can add up to 5 screenshots to your Bug Report. Do not abuse this function or you will be banned.</p>
-    <input type="file" accept="image/png, image/jpeg" multiple="multiple" onchange="loadFile(event)"><br>
-    <div id="output"></div>
-{:else}
-    <p>If your Verification level is at least "BASIC" you can also upload images.
-    Verification is done manually and at the discrecion of the admins only at the moment.</p>
-{/if}
-
-    <script>
-        const img = (src) => `<img src=${src} width="20%"/>`;
-
-        var loadFile = function (event) {
-          var output = document.getElementById('output');
-          output.innerHTML = '';
-
-          [...event.target.files].forEach(
-            (file) => (output.innerHTML += img(URL.createObjectURL(file)))
-          );
-        };
-    </script>
-
-    <script>
-    fileInput.addEventListener('change', function () {
-        for (let file of fileInput.files) {
-            attachments.append('documents', file);
-        }
-    });
-    </script>
-
-
+  <!-- TODO: Sending attachments broken, didn't understand documentation: https://pocketbase.io/docs/files-handling ^^' -->
+  {#if $authData?.baseModel?.verification != ""}
+    <h3>Screenshots (optional)</h3>
+    <p align="center">
+      You can add up to 5 screenshots to your Bug Report.<br />Do not abuse this
+      function or you will be banned.
+    </p>
+    <p align="center">
+      <label class='file-upload' for="attachments">
+        Add attachments
+      </label>
+      <input
+        bind:files={attachments}
+        id="attachments"
+        type="file"
+        name="attachments"
+        accept="image/png, image/jpeg"
+        multiple="multiple"
+      />
+    </p>
+    <div id="filesPreview">
+      {#if attachments}
+        {#each Array.from(attachments) as file, index}
+          <div class="previewRow">
+            <div
+              class="preview"
+              style={`background-image: url(${URL.createObjectURL(file)})`}
+            ></div>
+            <span class="fileName">{file.name}</span>
+            <span class="remove" on:click={() => removeAttachment(index)} />
+          </div>
+        {/each}
+      {/if}
+    </div>
+  {:else}
+    <p>
+      If your Verification level is at least "BASIC" you can also upload images.
+      Verification is done manually and at the discrecion of the admins only at
+      the moment.
+    </p>
+  {/if}
   <div class="actions">
     <button on:click={publishReport}>Submit</button>
   </div>
@@ -137,5 +183,37 @@
   }
   button {
     margin: 1em 0;
+  }
+  .previewRow {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-top: 1em;
+    justify-content: space-between;
+    row-gap: 1em;
+  }
+  .previewRow span.fileName {
+    max-width: calc(100vw - 7em - 32px - 96px);
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .previewRow span.remove {
+    background-image: url('/images/close.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+    margin: 0 2em;
+  }
+  .preview {
+    height: 96px;
+    width: 96px;
+    margin-right: 1em;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
   }
 </style>
