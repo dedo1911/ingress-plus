38c744d3f8bf0dfbe6c46d4b5202c17f1a258942
InvestigateXM <52758500+InvestigateXM@users.noreply.github.com>
2025-06-06 19:39:00 +0200

    v2 draft for updated tos

--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -6,14 +6,15 @@
   <h1>TERMS OF SERVICE</h1>
   <h3>Last updated June 06th, 2025</h3>
 
-  <h3>AGREEMENT TO OUR TERMS</h3>
+  <h2>AGREEMENT TO OUR TERMS</h2>
 
   <p>We are Ingress.Plus ("we," "us," "our" and similar).</p>
 
   <p>
     We operate the website <a href="https://ingress.plus"
       >https://ingress.plus</a
-    > (the "Site").
+    > (the "Site"). To use the Site you will need to agree to these Terms of Use which we may update
+    from time to time. We ask you to regularly check this page for any updates to these Terms that we may have posted.
   </p>
 
   <p>
@@ -26,7 +27,18 @@
     Ingress Plus is a non-commercial website that provides information and features
     in relation to Ingress Prime. Ingress Prime itself is a mobile application
     created by Niantic Spatial, Inc. and is not connected to Ingress Plus in any
-    way, be it technically or legally.
+    way, be it technically or legally. Ingress Plus does not share any data with Niantic Spatial, Inc.
+  </p>
+
+  <p>
+    We provide the Site and its features "as is" without any warranty of any kind.
+    We are not responsible for any damages, direct or indirect, that may result from the use or inability to use the Site.
+    We reserve the right to modify, suspend, or discontinue any part of the Site at any time without prior notice.
+  </p>
+
+  <p>
+    By using the Site, you agree that we may collect and process limited personal data as required to provide the service.
+    For more information, please refer to our Privacy Policy.
   </p>
 
   <p>
@@ -44,13 +56,16 @@
   </p>
 
   <p>
-    All users that submit user generated content (text, images and similar) may not submit
-    content that is ilegal, racist or similar. We ask all users to follow common sense.
+    All users that submit user-generated content (text, images and similar) may not submit
+    content that is illegal, racist or similar. Users are also not allowed to post content that
+    defames or harass another person (such as accusing someone of cheating). We ask all users to follow common sense.
   </p>
 
   <p>
     The admins of the site can, at their own discretion, either restrict or ban a user from the site.
     This right will be exercised especially when these Terms of Service are broken.
+    We reserve the right to take any action we deem necessary to protect the Site and its users,
+    including removing content or restricting access to the Site.
   </p>
 
 </div>
